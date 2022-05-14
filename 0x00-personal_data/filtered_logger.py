66c39271235ae81fc4eb5c993fbd67eb2904114b
Obed Amoako <obedamoako92@gmail.com>
2022-05-14 18:02:43 +0000

    added user data obfuscator

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,11 +13,3 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         repl = field + "=" + redaction + seperator
         message = re.sub(pattern=pattern, repl=repl, string=message)
     return message
-
-
-fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-            "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
