02dcad5ad78d3ba7331fdf5db4d70b18b6ebab5c
Obed Amoako <obedamoako92@gmail.com>
2022-05-14 20:29:10 +0000

    modified filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ This module filters user data """
 
+import logging
 import re
 from typing import List
 
@@ -12,3 +13,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         pattern = field + "=.*?" + seperator
         repl = field + "=" + redaction + seperator
     return re.sub(pattern=pattern, repl=repl, string=message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Instance initializer"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Describes the format"""
+        NotImplementedError
