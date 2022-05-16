47e94fa3e727cf08321c5cf15cbe32d0c6982f70
Obed Amoako <obedamoako92@gmail.com>
2022-05-16 10:46:19 +0000

    added custom frobidden error

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -30,3 +30,8 @@ def stats() -> str:
 def unauthorized():
   """Returns a 401 error"""
   abort(401)
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden():
+  """Returns a 403 error"""
+  abort(403)
