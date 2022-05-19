d1e22f1e9daaa04c04c0d27e7c3c4ac4d374f300
Obed Amoako <obedamoako92@gmail.com>
2022-05-19 20:38:47 +0000

    retrieving the whole user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,3 +65,12 @@ class BasicAuth(Auth):
             return None
         for user in users:
             return user if User.is_valid_password(user_pwd) else None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Gets the whole user instance"""
+        header = self.authorization_header(request)
+        base64 = self.extract_base64_authorization_header(header)
+        decoded = self.decode_base64_authorization_header(base64)
+        credentials = self.extract_user_credentials(decoded)
+        user = self.user_object_from_credentials(*credentials)
+        return user
