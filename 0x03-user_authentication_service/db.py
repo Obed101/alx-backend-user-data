ff070c3246fd43922ad9e7e16f04d50bbe75468d
Obed Amoako <obedamoako92@gmail.com>
2022-05-27 17:56:31 +0000

    changed import

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,8 +2,8 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
-# from sqlalchemy.orm.exc import NoresultFound
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoresultFound
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
