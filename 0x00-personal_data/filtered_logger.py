c8c43fdb840a768cb197b0d0e77f80a0d569aad7
Obed Amoako <obedamoako92@gmail.com>
2022-05-27 11:49:42 +0000

    connecting to database to get user data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,16 +2,20 @@
 """ This module filters user data """
 
 import logging
+from multiprocessing import connection
+from os import environ
 import re
 from typing import List
 
+PII_FIELDS = ('database', 'email', 'phone', 'password', 'ssn')
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str) -> str:
+                 separator: str) -> str:
     """filters datum with regex"""
     for field in fields:
-        pattern = field + "=.*?" + seperator
-        repl = field + "=" + redaction + seperator
+        pattern = field + "=.*?" + separator
+        repl = field + "=" + redaction + separator
     return re.sub(pattern=pattern, repl=repl, string=message)
 
 
@@ -21,7 +25,7 @@ class RedactingFormatter(logging.Formatter):
     """
 
     REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT = "[HOLBERTON] %(database)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
@@ -30,5 +34,32 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Describes the format"""
-        pass
+        """uses filter_datum to format data"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """returns a Logger object from user data"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> connection.MySQLConnection:
+    """Connects securely to a database to read User Data"""
+    database = environ.get("PERSONAL_DATA_DB_NAME")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+
+    return connection.MySQLConnection(user=username, password=password,
+                                           host=host, database=database)
+
