5b8f6ee655846abc3d6a225efb607c6abefc9dda
Obed101 <obedamoako92@gmail.com>
2022-05-27 12:00:00 +0000

    changed file mode

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """ This module filters user data """
 
 import logging
-from multiprocessing import connection
+from mysql.connector.connection import connection
 from os import environ
 import re
 from typing import List
