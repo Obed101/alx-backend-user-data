6bd2011cbcc92aeee82f68969d03e59197f1cb4e
Obed Amoako <obedamoako92@gmail.com>
2022-06-01 07:29:20 +0000

    added driver code for all

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""main module for testing the functions and the whole app"""
+
+import requests
+
+url = 'http://0.0.0.0:5000'
+
+def register_user(email: str, password: str) -> None:
+    """test register method"""
+    credencials = {"email": email, "password": password}
+    response = requests.post(f'{url}/users', data=credencials)
+    assert response.status_code == 200, "Test fail"
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
